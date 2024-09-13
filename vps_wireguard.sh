ce04679b274a50b201cee188a3d44d56f85a613d
Léo Ruer <126884638+LeoRuer@users.noreply.github.com>
2024-09-13 13:36:42 +0200

    Update vps_wireguard.sh

--- vps_wireguard.sh
+++ vps_wireguard.sh
@@ -9,7 +9,7 @@ WG_SERVER_PUBLIC_KEY="<contenu_de_proxmox_public.key>"
 WG_SERVER_ENDPOINT="<adresse_ip_de_proxmox>:$WG_PORT"
 
 # Installer WireGuard
-apt update
+apt update && apt upgrade -y
 apt install -y wireguard
 
 # Générer les clés
@@ -40,7 +40,12 @@ systemctl start wg-quick@$WG_INTERFACE
 #Interface web Proxmox depuis l'IP du VPS
 iptables -t nat -A PREROUTING -p tcp --dport 8006 -j DNAT --to-destination 10.0.0.1:8006
 iptables -A FORWARD -p tcp -d 10.0.0.1 --dport 8006 -j ACCEPT
-#ne pas oublier de remplacer les IP et d'autoriser le firewall ionos
+#Ne pas oublier de remplacer les IP et d'autoriser le firewall ionos
+
+#Sauvegarder les règles iptables
+apt install iptables-persistent
+netfilter-persistent save
+
 
 
 echo "Configuration du client WireGuard terminée."
